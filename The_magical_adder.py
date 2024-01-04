a957f964974ca5222abad0fc10e01f269941b208
Asmita Ojha <asmitaojha05@gmail.com>
2024-01-04 13:48:37 +0545

    magical adder

---
+++ The_magical_adder.py
@@ -0,0 +1,15 @@
+user_input = input("Please enter three numbers separated by comma:")
+string_tokens = user_input.split(",")
+#convert the tokens intp integers
+int_tokens = []
+for st in string_tokens:
+    int_tokens.append(int(st))
+
+#calculate the result : a+b-c
+a,b,c = int_tokens
+result = a+b-c
+#result = int_tokens[0] + int_tokens[1]-int_tokens[2]
+
+
+#output the result
+print(result)
