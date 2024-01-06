4f7ccf3f0f4551b49a7c10b070711b201e9a9abb
Asmita Ojha <asmitaojha05@gmail.com>
2024-01-06 18:00:51 +0545

    banner modified

--- banner.py
+++ banner.py
@@ -23,3 +23,9 @@ banner_text("Don't be sily chumps,")
 banner_text("Just purse your lips and whistle - that's the thing!")
 banner_text("And ... always look on the bright side of life...")
 banner_text("*")
+result = banner_text("nothing is returned")
+print(result)
+
+numbers = [4,2,7,5,8,3,9,6,1]
+print(numbers.sort()) # This will print None, as the sort() method modifies the list in-place and returns None.
+print(numbers)
